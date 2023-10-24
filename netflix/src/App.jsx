8180f87228bdfdd13a30779374f25490d59f09c8
RUDRANSHFLY <rokudokubaradiyabrothers@gmail.com>
2023-10-24 16:29:35 +0530

    HomeScreen Page Add in App

--- netflix/src/App.jsx
+++ netflix/src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import HomeScreen from './pages/HomeScreen/HomeScreen'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-
-    </>
+    <div className="app">
+      <HomeScreen />
+    </div>
   )
 }
 
