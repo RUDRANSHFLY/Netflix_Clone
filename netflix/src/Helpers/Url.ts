e63d15135d56d9b189eaae0cc0a91eba02b1a3ad
RUDRANSHFLY <rokudokubaradiyabrothers@gmail.com>
2023-10-24 20:28:03 +0530

    Netflix logo and Profile Svg Add

--- netflix/src/Helpers/Url.ts
+++ netflix/src/Helpers/Url.ts
@@ -1,1 +1,3 @@
 export const AppLogo : String = "https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" ; 
+
+export const AppProfileLogo : String = "https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png";
