7a00dc9909b226da543cd8a2e3b1f75914b99493
RUDRANSHFLY <rokudokubaradiyabrothers@gmail.com>
2023-10-26 12:43:47 +0530

    Change in App Base Url

--- netflix/src/Helpers/Url.ts
+++ netflix/src/Helpers/Url.ts
@@ -1,3 +1,7 @@
 export const AppLogo : String = "https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" ; 
 
 export const AppProfileLogo : String = "https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png";
+
+const ApiId : Number = 20624530 ;
+
+export const ApiBaseUrl : string = `https://api.themoviedb.org/3/`;
