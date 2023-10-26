8d288726eab6f8b99cbf666d2ef023e6d5817c9a
RUDRANSHFLY <rokudokubaradiyabrothers@gmail.com>
2023-10-26 12:45:58 +0530

    Authorizattion => authorization

--- netflix/src/Network/Api/axios.ts
+++ netflix/src/Network/Api/axios.ts
@@ -1,8 +1,14 @@
 import axios from 'axios';
+import { ApiBaseUrl } from '../../Helpers/Url';
+
+const apiToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NTNlMGNhODUxY2I2YTM5MzRmMTU1NTMyYjE4NmMyMCIsInN1YiI6IjY1MzllYTk3Njc4MjU5MDBjN2U4NDhjMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zAyoNwjItHBUfwRC7udpUqU6yaIHKFCpmfvLPCJv5Ts" ;
 
 const Axios = axios.create(
     {
-        baseURL : "",
+        baseURL : ApiBaseUrl,
+        headers : {
+            authorization : "bearer " + apiToken ,
+        },
     }
 ); 
 
